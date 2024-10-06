274a1a4b7246f0b00d255cce303d6e9b3ccf19da
Leonardo Rincon <jleobotache@gmail.com>
2024-10-06 15:45:24 -0500

    add the crud to users

--- src/pages/script/profile.js
+++ src/pages/script/profile.js
@@ -1,13 +1,1 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.getElementById("add-user")
-const closeButton = document.querySelector("dialog button");
-
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-    dialog.close();
-});
+// js para las interacciones de la página de perfil
